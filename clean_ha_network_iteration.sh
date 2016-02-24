1fb247e16fadf81588ab7d2b67c669db47980289
bkopilov <bkopilov@redhat.com>
2016-02-24 17:20:29 +0200

    add HA network clean up

---
+++ clean_ha_network_iteration.sh
@@ -0,0 +1,19 @@
+#!bin/bash
+
+# HA network not deleted when router got deleted.
+# This is a known bug and breaks tempest runs
+SLEEP_TIME=60
+KEYSTONE_RC=/home/stack/overcloudrc
+EXCLUDE="HA network|\^\$"
+COLUM_RETURN='{print $2}'
+source $KEYSTONE_RC
+while :
+do
+  network_ha=`neutron net-list | grep -E "$EXCLUDE" |  awk "$COLUM_RETURN"`
+  for i in ${network_ha} ; do neutron net-delete ${i} ;  done ;
+  echo Going to sleep $SLEEP_TIME seconds
+  sleep $SLEEP_TIME
+done
+
+
+#
