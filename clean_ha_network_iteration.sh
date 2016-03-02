6baca554db97e1f74de19eb5b9df5bbedd79d81a
bkopilov <bkopilov@redhat.com>
2016-03-02 12:05:01 +0200

    fix HA network delete script

--- clean_ha_network_iteration.sh
+++ clean_ha_network_iteration.sh
@@ -3,12 +3,12 @@
 # This is a known bug and breaks tempest runs
 SLEEP_TIME=60
 KEYSTONE_RC=/home/stack/overcloudrc
-EXCLUDE="HA network|\^\$"
+INCLUDE="HA network|\^\$"
 COLUM_RETURN='{print $2}'
 source $KEYSTONE_RC
 while :
 do
-  network_ha=`neutron net-list | grep -E "$EXCLUDE" |  awk "$COLUM_RETURN"`
+  network_ha=`neutron net-list | grep -E "$INCLUDE" |  awk "$COLUM_RETURN"`
   for i in ${network_ha} ; do neutron net-delete ${i} ;  done ;
   echo Going to sleep $SLEEP_TIME seconds
   sleep $SLEEP_TIME
